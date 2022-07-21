dd60c63b05a05765123ad565dfcb65387483b607
MarcoBorgess <marco.borges@branef.com.br>
2022-07-21 17:36:20 -0300

    teste em prod lesgooo

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import bazaarUpdater as bz
 import binsUpdater as bins
 import itemInfoUpdater as itemInfo
 from time import sleep
+from datetime import date
+
+today = date.today().strftime('%d')
+lastDay = 0
 
 while True:
     bz.updateBazaar()
     bins.updateBins()
-    itemInfo.updateItemsInfo()
+    if (today != lastDay):  
+        itemInfo.updateItemsInfo()
+        lastDay = today
     print('NOW SLEEPING 💤')
     sleep(900)
