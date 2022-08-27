226b6f47f75e830b80de16a82e66fd2b1ba151f2
MarcoBorgess <marco.borges@branef.com.br>
2022-08-27 11:48:25 -0300

    fds tudo

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ today = date.today().strftime('%d')
 lastDay = 0
 
 while True:
-    # bz.updateBz()
-    # ah.updateAh()
-    img.update_icon64_column()
+    bz.updateBz()
+    ah.updateAh()
+    # img.update_icon64_column()
     
-    # if (today != lastDay):
-    #     print('⌛ Updating items table, gonna take a while...')
-    #     item.updateItemTable()
-    #     lastDay = today
+    if (today != lastDay):
+        print('⌛ Updating items table, gonna take a while...')
+        item.updateItemTable()
+        lastDay = today
         
     print('💤 NOW SLEEPING 💤')
     sleep(900)
